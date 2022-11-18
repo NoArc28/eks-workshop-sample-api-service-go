d5cb2124a475f8afb5e2401e8ad86be331b8098d
NoArc28 <91405419+NoArc28@users.noreply.github.com>
2022-11-18 17:48:35 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
  
 func main() {   
-    http.Handle("/", http.FileServer(http.Dir("html")))
+    http.Handle("/", http.FileServer(http.Dir("/greet/html")))
     //http.Handle("/static", http.FileServer(http.Dir("wwwroot")))
     http.ListenAndServe(":8080", nil)
 }
