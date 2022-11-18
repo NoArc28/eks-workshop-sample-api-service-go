07ff530438ea5ba68297852e62ebf72e9565eefa
NoArc28 <91405419+NoArc28@users.noreply.github.com>
2022-11-18 19:33:55 +0900

    Update main.go

--- main.go
+++ main.go
@@ -10,11 +10,6 @@ func main() {
       fmt.Fprintf(w, "Hyeonho aws test success!!")
    })
 
-   http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-      name := r.URL.Path[len("/greet/"):]
-      fmt.Fprintf(w, "Hello %s\n", name)
-   })
-
    http.ListenAndServe(":8080", nil)
 
 }
