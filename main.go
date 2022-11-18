e49ed90ee5d90f376ca762576077e4eb8e35b03f
NoArc28 <91405419+NoArc28@users.noreply.github.com>
2022-11-18 16:58:02 +0900

    Update main.go

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
    "net/http"
 )
 
-func main() {
-   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-      fmt.Fprintf(w, "Hyeonho aws test success!!")
-   })
+func  main(){
 
-   http.HandleFunc("/greet/", func(w http.ResponseWriter, r *http.Request) {
-      name := r.URL.Path[len("/greet/"):]
-      fmt.Fprintf(w, "Hello %s\n", name)
-   })
+	http.HandleFunc("/html", uploadsHandler)
+/* 업로드 경로 에 대한 핸들러를 만들어줌 */ 
 
-   http.ListenAndServe(":8080", nil)
 
+	http.Handle("/",http.FileServer(http.Dir("public")))
+/* 인덱스 경로 - 파일서버 (퍼블릭 경로) */
+ 
+	http.LisetAndServe(":3000",nil)
+
+/* 가장 고전적인 파일 웹서버 - public 폴더 안에 있는 파일들을 엑세스 할수 있도록 열어주는것 */
 }
